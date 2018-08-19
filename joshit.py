e64e03cdb98fc5faf051577b03dbda845c90cab1
Eros <george.marr.cobc@gmail.com>
2018-08-19 16:43:30 +0100

    okay i get too bored

--- joshit.py
+++ joshit.py
@@ -23,3 +23,16 @@ def josh_it(word: '', strength: int):
     return letters.join("").trim()
 
 
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-w', help='the word to implement')
+    parser.add_argument('-s', help='strength you want to fuck it up with ')
+    args = parser.parse_args()
+    if '-w' or '-s' not in args :
+        print(parser.usage)
+    else:
+        josh_it(args.w, args.s)
+
+
+if __name__ == '__main__':
+    main()
